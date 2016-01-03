3af9681689d05ad31921ae122a921bbc84bb932c
Marcel Klehr <mklehr@gmx.net>
2016-01-03 18:09:05 +0100

    Refactor to work with v0.4 redux-based ui

--- client.js
+++ client.js
@@ -7,17 +7,12 @@ function setup(plugin, imports, register) {
   var editor = imports.editor
 
   editor.registerEditor('Textarea', 'text', 'Just a textarea, nothing more'
-  , function*(el) {
+  , function(el) {
     var textarea = document.createElement('textarea')
     textarea.setAttribute('id', 'content')
     el.appendChild(textarea)
 
     el.style['height'] = '100%'
-    document.body.style['position'] = 'absolute'
-    document.body.style['bottom'] =
-    document.body.style['top'] =
-    document.body.style['left'] =
-    document.body.style['right'] = '0'
     textarea.style['height'] = '100%'
     textarea.style['width'] = '100%'
     textarea.style['border'] = '0'
--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var path = require('path')
   , textOT = require('ot-text').type
 
 module.exports = setup
-module.exports.consumes = ['assets', 'ot']
+module.exports.consumes = ['ui', 'ot']
 
 function setup(plugin, imports, register) {
-  var assets = imports.assets
+  var ui = imports.ui
   var ot = imports.ot
 
-  assets.registerModule(path.join(__dirname, 'client.js'))
-  
+  ui.registerModule(path.join(__dirname, 'client.js'))
+
   ot.registerOTType('text', textOT)
 
   register()
