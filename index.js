e6eee88059d773e27802bc0bef535dcc0c7cb288
Marcel Klehr <mklehr@gmx.net>
2016-01-16 15:45:34 +0100

    Implement export text/plain

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ var path = require('path')
   , textOT = require('ot-text').type
 
 module.exports = setup
-module.exports.consumes = ['ui', 'ot']
+module.exports.consumes = ['ui', 'ot', 'importexport']
 
 function setup(plugin, imports, register) {
   var ui = imports.ui
-  var ot = imports.ot
+    , ot = imports.ot
+    , importexport = imports.importexport
 
   ui.registerModule(path.join(__dirname, 'client.js'))
 
   ot.registerOTType('text/plain', textOT)
+
+  importexport.registerExportProvider('text/plain', 'text/plain'
+  , function* (document, snapshot) {
+    return snapshot.contents
+  })
 
   register()
 }
